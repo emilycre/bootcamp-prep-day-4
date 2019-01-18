2ca8623df80d8de30cde94d7dd84169dddc56e2d
Emily Creamer <emily.creamer@icloud.com>
2019-01-17 19:45:59 -0800

    making greeting!

--- main.js
+++ main.js
@@ -1,6 +1,24 @@
 // eslint-disable-next-line
 function greet() {
-    console.log('greet function called');
+    
+    // get the names input by ID
+    var firstNameInput = document.getElementById('first-name');
+    var lastNameInput = document.getElementById('last-name');
+    
+    // get values out of the inputs
+    var firstName = firstNameInput.value;
+    var lastName = lastNameInput.value;
+    
+    // make greeting
+    var greeting = 'Hello, ' + firstName +  lastName + '!';
+    console.log(greeting);
+
+    // display greeting
+
+
+    // remove prompt
+
 }
 
-greet();
+// TODO: validate name inputs
+// TODO: clear inputs after greet?
